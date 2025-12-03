47a489d29e91b7e7803c88ce5dd957bb809f58cf
Nacho02s <74149764+Nacho02s@users.noreply.github.com>
2025-12-03 17:47:54 +0100

    render

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def ejecutar_dashboard():
     print("Presiona Ctrl+C para detener el servidor")
     print("=" * 60 + "\n")
     
-    app.run(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=True, host='0.0.0.0', port=10000)
 
 
 
